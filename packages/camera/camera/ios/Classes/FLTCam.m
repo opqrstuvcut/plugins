5be907727205a5a01459b8e00fc42085afe64bd0
opqrstuvcut <vikz2713@gmail.com>
2022-05-28 09:58:35 +0900

    Update FLTCam.m

--- packages/camera/camera/ios/Classes/FLTCam.m
+++ packages/camera/camera/ios/Classes/FLTCam.m
@@ -1015,8 +1015,9 @@ NSString *const errorMethod = @"error";
     return NO;
   }
 
-  NSDictionary *videoSettings = [_captureVideoOutput
-      recommendedVideoSettingsForAssetWriterWithOutputFileType:AVFileTypeMPEG4];
+  NSDictionary *videoSettings = 
+      [_captureVideoOutput recommendedVideoSettingsForVideoCodecType:AVVideoCodecTypeH264
+                                              assetWriterOutputFileType:AVFileTypeMPEG4];
   _videoWriterInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo
                                                          outputSettings:videoSettings];
 
